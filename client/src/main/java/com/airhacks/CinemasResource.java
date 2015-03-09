ccca9784a307a684319276e63beebb271c02bb51
Adam Bien <abien@adam-bien.com>
2015-03-09 20:29:37 +0100

    Async, but more stable

--- client/src/main/java/com/airhacks/CinemasResource.java
+++ client/src/main/java/com/airhacks/CinemasResource.java
@@ -1,7 +1,9 @@
 package com.airhacks;
 
 import java.util.concurrent.CompletableFuture;
+import javax.annotation.Resource;
 import javax.ejb.Stateless;
+import javax.enterprise.concurrent.ManagedExecutorService;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -20,9 +22,12 @@ public class CinemasResource {
     @Inject
     WebTarget target;
 
+    @Resource
+    ManagedExecutorService mes;
+
     @GET
     public void all(@Suspended AsyncResponse response) {
-        CompletableFuture.supplyAsync(this::getMovies).
+        CompletableFuture.supplyAsync(this::getMovies, mes).
                 thenAccept(response::resume);
     }
 
