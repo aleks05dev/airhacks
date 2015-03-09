fe16fb21b63adf31c786e672f13867d56007ae6f
Adam Bien <abien@adam-bien.com>
2015-03-09 20:41:33 +0100

    Timeout (evil attendee)

--- service/src/main/java/com/airhacks/MoviesResource.java
+++ service/src/main/java/com/airhacks/MoviesResource.java
@@ -1,5 +1,7 @@
 package com.airhacks;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
@@ -12,6 +14,11 @@ public class MoviesResource {
 
     @GET
     public String movies() {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(MoviesResource.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return "matrix";
     }
 
